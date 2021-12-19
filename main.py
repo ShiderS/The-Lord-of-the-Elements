14358a2ee5a363e497e70529efa173951f77053d
ShiderS <irbis26813@gmail.com>
2021-12-19 20:19:32 +0500

    displaying textures

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from textures import *
 if __name__ == '__main__':
     pygame.init()
     clock = pygame.time.Clock()
-    all_sprites = pygame.sprite.Group()
 
     pygame.display.set_caption('Mygame')
     size = 1000, 800
