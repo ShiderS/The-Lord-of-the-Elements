405be55cdbb5c90ffae72fa9f950aeed0e231472
ShiderS <irbis26813@gmail.com>
2022-01-08 12:20:30 +0500

    added start screen

--- main.py
+++ main.py
@@ -62,7 +62,34 @@ class Camera:
 
 camera = Camera()
 
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), size)
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+
 if __name__ == '__main__':
+    # start_screen()
     info_images = Level_characteristics(size[0], size[1], level, 'info_level.txt').render()
     info_images_radiation = Level_characteristics(size[0], size[1], level, 'info_radiation.txt').render()
 
@@ -91,6 +118,7 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                terminate()
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
